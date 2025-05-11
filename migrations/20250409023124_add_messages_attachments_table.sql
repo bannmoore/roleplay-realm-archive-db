03052f9a0a91829ee2541d8f54e5248fcdb093bd
bannmoore <moore.brittanyann@gmail.com>
2025-05-11 10:20:16 -0700

    Replace uri fields with path in attachments.

--- migrations/20250409023124_add_messages_attachments_table.sql
+++ migrations/20250409023124_add_messages_attachments_table.sql
@@ -4,8 +4,7 @@ CREATE TABLE messages_attachments (
     id bigserial PRIMARY KEY,
     discord_id text UNIQUE NOT NULL,
     message_id bigint REFERENCES messages(id) ON DELETE CASCADE NOT NULL,
-    source_uri text,
-    discord_source_uri text NOT NULL,
+    storage_path text NOT NULL,
     width integer,
     height integer,
     created_at timestamp NOT NULL DEFAULT now()
