884508f5712964a61db2c95ffe8310f4403cb042
bannmoore <moore.brittanyann@gmail.com>
2026-01-08 19:18:55 -0800

    Add users.is_admin column.

---
+++ migrations/00011_add_users_is_admin_column.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE users
+  ADD COLUMN is_admin boolean NOT NULL DEFAULT FALSE;
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE users
+  DROP COLUMN is_admin;
+
+-- +goose StatementEnd
